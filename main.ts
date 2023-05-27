704b0b6324b79b80622b1de1cd9d390a0ab2af55
MRSLIMECODING <121568700+MRSLIMECODING@users.noreply.github.com>
2023-05-26 23:21:45 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    logging = !(logging)
+    if (true) {
+        basic.showIcon(IconNames.Target)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.clearScreen()
+    }
+})
+let logging = false
+logging = false
+loops.everyInterval(60000, function () {
+    if (logging) {
+        datalogger.log(
+        datalogger.createCV("temp", input.temperature()),
+        datalogger.createCV("light", input.lightLevel())
+        )
+    }
+})
+basic.forever(function () {
+	
+})
