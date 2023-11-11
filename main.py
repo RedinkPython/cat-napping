7128953d3f487aae81ed4b33b112fef6118cc33d
Cesar Gabriel <121568700+RedinkPython@users.noreply.github.com>
2023-11-10 18:15:24 -0600

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ def on_button_pressed_a():
         basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+"""
+
+This wa made with Python
+
+"""
 logging = False
 logging = False
+Note
 
 def on_every_interval():
     if logging:
         datalogger.log(datalogger.create_cv("temp", input.temperature()),
             datalogger.create_cv("light", input.light_level()))
 loops.every_interval(60000, on_every_interval)
-
-Note this was updated to Python
--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    logging = !logging
+input.onButtonPressed(Button.A, function () {
+    logging = !(logging)
     if (true) {
         basic.showIcon(IconNames.Target)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
     } else {
         basic.clearScreen()
     }
-    
 })
+/**
+ * Tis was made with Python, if you see this you are in code blocks and not in python
+ */
 let logging = false
 logging = false
-loops.everyInterval(60000, function on_every_interval() {
-    if (logging) {
-        datalogger.log(datalogger.createCV("temp", input.temperature()), datalogger.createCV("light", input.lightLevel()))
-    }
-    
-})
 Note
+loops.everyInterval(60000, function () {
+    if (logging) {
+        datalogger.log(
+        datalogger.createCV("temp", input.temperature()),
+        datalogger.createCV("light", input.lightLevel())
+        )
+    }
+})
